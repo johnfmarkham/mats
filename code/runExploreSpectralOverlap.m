7bde572a72333b4d5f1c0e32db33701b1a5f1950
Andrey Kan <mr.ndry@gmail.com>
2015-03-31 13:16:05 +1100

    updated code for spectral overlap analysis

--- code/runExploreSpectralOverlap.m
+++ code/runExploreSpectralOverlap.m
@@ -1,4 +1,50 @@
-function unmixingParams = findSpectralUnmixingParams(experimentDetails)
+function runExploreSpectralOverlap()
+clear variables; close all; clc;
+inPath = fullfile('..', '..', '..');
+runPatt = '*-%04d';
+posPatt = '*%d*.tif_Files';
+ch1Patt = '*%dz*c01.tif';
+ch2Patt = '*%dz*c02.tif';
+
+allRun = [9 9     11 11   13 13   13 13  13 13    13 13    13 13];
+allFrm = [1 1     5 5     5 5     50 50  100 100  150 150  230 230];
+allPos = [36 184  257 54  78 229  113 3  60 144   192 57   3 119];
+numCases = numel(allRun);
+
+ctx.thLo = 10;
+ctx.thHi = 90;
+ctx.xBins = 100;
+ctx.sampleSize = 20000;
+ctx.outPath = fullfile('..', '..', 'unmixing');
+
+for iCase = 1:numCases
+	fprintf('--- case %d out of %d ---\n', iCase, numCases);
+	
+	pos = allPos(iCase);
+	run = allRun(iCase);
+	frm = allFrm(iCase);
+	
+	runDir = dir(fullfile(inPath, sprintf(runPatt, run)));
+	runDir = runDir(1).name;
+	
+	posDir = dir(fullfile(inPath, runDir, sprintf(posPatt, pos)));
+	posDir = posDir(1).name;
+	
+	nameChan1 = dir(fullfile(inPath, runDir, posDir, sprintf(ch1Patt, frm)));
+	nameChan1 = nameChan1(1).name;
+	nameChan2 = dir(fullfile(inPath, runDir, posDir, sprintf(ch2Patt, frm)));
+	nameChan2 = nameChan2(1).name;
+	
+	im1 = double( imread(fullfile(inPath, runDir, posDir, nameChan1)) );
+	im2 = double( imread(fullfile(inPath, runDir, posDir, nameChan2)) );
+	
+	ctx.name = sprintf('pos = %d, [run, frm] = [%d, %d]', pos, run, frm);
+	processCase(ctx, im1, im2)
+end
+
+%{
+experimentDetails.channel11
+
 debug = 1;
 
 im11 = imread(experimentDetails.channel11);
@@ -57,10 +103,58 @@ fid = fopen(experimentDetails.outfile,'w');
 fprintf(fid,'%f %f\n',p1(1),p2(1));
 %fprintf(fid,'%f %f\n',-minvec(1),-minvec(2));
 fclose(fid);
+%}
+end
 
-%-----------------------------------------
+function processCase(ctx, im1, im2)
+reset(RandStream.getGlobalStream, 2015);
+
+% figure; imshow(mat2gray(im1));
+% figure; imshow(mat2gray(im2));
+
+xvec = im1(:);
+yvec = im2(:);
+thLo1 = prctile(xvec, ctx.thLo);
+thLo2 = prctile(yvec, ctx.thLo);
+thHi1 = prctile(xvec, ctx.thHi);
+thHi2 = prctile(yvec, ctx.thHi);
+
+sel = (xvec < thLo1) | (yvec < thLo2) | (xvec > thHi1) | (yvec > thHi2);
+xvec = xvec(sel);
+yvec = yvec(sel);
+
+xvec = randsample(xvec, ctx.sampleSize);
+yvec = randsample(yvec, ctx.sampleSize);
+
+figure('name', ctx.name, 'position', [400, 400, 1200, 400]);
+subplot(1, 2, 1);
+densityPlot2D(xvec, yvec, [ctx.xBins, ctx.xBins])
+
+subplot(1, 2, 2);
+scatter(xvec, yvec);
+
+set(gcf, 'PaperPosition', [0 0 30 10]);
+set(gcf, 'PaperSize', [30 10]);
+saveas(gcf, fullfile(ctx.outPath, ctx.name), 'pdf');
+end
+
+function densityPlot2D(xvec, yvec, nbins)
+xvec = xvec(:);
+yvec = yvec(:);
+
+if (nargin < 3)
+	nbins = [51, 51];
+end
+values = hist3([xvec, yvec], 'nbins', nbins);
+values = log(values + 0.0001);
+
+imagesc(values');
+colorbar;
+axis xy;
+end
+
+%{
 function result = cost(v,m11,m12,m21,m22)
-%-----------------------------------------
 mix_12 = v(2);
 mix_21 = v(1);
 s11 = double(m11) + mix_12 * double(m21);
@@ -75,6 +169,5 @@ p2 = polyfit(double(s22),double(s12),1);
 %result = std(s21) +  std(s12);
 result = abs(p1(1)) + abs(p2(1));
 fprintf(1,'%f %f %f %f %f\n',result,v(1),v(2),p1(1),p2(1));
-
-
-
+end
+%}
